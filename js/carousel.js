9eb768ce0b5d60292921f668592a36d44099b64f
smmkDev <mkaz9857@gmail.com>
2023-07-25 18:59:38 +0500

    first commit

---
+++ js/carousel.js
@@ -0,0 +1,45 @@
+// Global Variables
+var intro_arr = document.getElementsByClassName("intro");
+var next = document.getElementById("next")
+var prev = document.getElementById("prev");
+var slide_idx = 0;
+
+function hide()
+{
+    for(let i=1; i<intro_arr.length; i++)
+    {
+        intro_arr[i].style.display = "none";
+    }
+}
+
+function next_slide()
+{
+   if(slide_idx < intro_arr.length - 1)
+   {
+        intro_arr[slide_idx].style.display = "none";
+        slide_idx++;
+        intro_arr[slide_idx].style.display = "block";
+   }
+}
+
+function prev_slide()
+{
+   if(slide_idx - 1 >= 0)
+   {
+        intro_arr[slide_idx].style.display = "none";
+        slide_idx--;
+        intro_arr[slide_idx].style.display = "block";
+   }
+}
+
+hide();
+next.addEventListener
+(
+    "click",
+    next_slide
+)
+prev.addEventListener
+(
+    "click",
+    prev_slide
+)
